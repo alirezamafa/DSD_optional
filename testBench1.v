2f39bd30bc571947dcc149accf79845e1960a052
alirezamafa <122224802+alirezamafa@users.noreply.github.com>
2024-06-26 23:55:46 +0330

    Add files via upload

--- testBench1.v
+++ testBench1.v
@@ -4,7 +4,7 @@ reg[4:0] input_data;
 reg[2:0] opcode;
 wire[4:0] output_data;
 wire overflow;
-stack_based_alu(.input_data(input_data),.output_data(output_data),.overflow(overflow).opcode(opcode));
+stack_base_alu #(5) stack_base_alu_5bit(.input_data(input_data),.output_data(output_data),.overflow(overflow).opcode(opcode));
 initial begin
     input_data = 5'b10110;
     opcode = 3'b110;
@@ -25,12 +25,12 @@ initial begin
     #5
     input_data=5'b01010;
     #5
-    opcode=3'101;
+    opcode=3'b101;
     #5
-    input_data = 5'00010;
-    opcode=3'110;
+    input_data = 5'b00010;
+    opcode=3'b110;
     #5
-    input_data=5'11011;
+    input_data=5'b11011;
     #5
     opcode=3'b101;
     #5
--- testBench2.v
+++ testBench2.v
@@ -13,10 +13,10 @@ wire overflow_16bit;
 reg[31:0] input_data_32bit;
 wire[31:0] output_data_32bit;
 wire overflow_32bit;
-stack_based_alu #(4) stack_based_alu_4bit(.input_data(input_data_4bit),.opcode(opcode),.overflow(overflow_4bit),.output_data(output_data_4bit));
-stack_based_alu #(8) stack_based_alu_8bit(.input_data(input_data_8bit),.opcode(opcode),.overflow(overflow_8bit),.output_data(output_data_8bit));
-stack_based_alu #(16) stack_based_alu_16bit(.input_data(input_data_16bit),.opcode(opcode),.overflow(overflow_16bit),.output_data(output_data_16bit));
-stack_based_alu #(32) stack_based_alu_32bit(.input_data(input_data_32bit),.opcode(opcode),.overflow(overflow_32bit),.output_data(output_data_32bit));
+stack_base_alu #(4) stack_based_alu_4bit(.input_data(input_data_4bit),.opcode(opcode),.overflow(overflow_4bit),.output_data(output_data_4bit));
+stack_base_alu #(8) stack_based_alu_8bit(.input_data(input_data_8bit),.opcode(opcode),.overflow(overflow_8bit),.output_data(output_data_8bit));
+stack_base_alu #(16) stack_based_alu_16bit(.input_data(input_data_16bit),.opcode(opcode),.overflow(overflow_16bit),.output_data(output_data_16bit));
+stack_base_alu #(32) stack_based_alu_32bit(.input_data(input_data_32bit),.opcode(opcode),.overflow(overflow_32bit),.output_data(output_data_32bit));
 initial begin
    input_data_4bit=4'b0011;
    opcode =3'b110;
