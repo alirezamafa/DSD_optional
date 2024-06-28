c167f1624da5575f8c4ea1b7dc6c486a54ce2630
alirezamafa <122224802+alirezamafa@users.noreply.github.com>
2024-06-28 16:55:34 +0330

    Add files via upload

--- stack_based_alu.v
+++ stack_based_alu.v
@@ -8,17 +8,8 @@ reg signed [2*N-1:0] checking_of;
 reg signed [2*N-1:0] extended_reg;
 
 always @(*)begin
-    //addition
-    if(opcode==3'b100 && (pointer>=2))begin
-        output_data=stack[pointer-1]+stack[pointer-2];
-        if((stack[pointer-1][N-1]==stack[pointer-2][N-1]) && (output_data[N-1]!= stack[pointer-1][N-1]))begin
-            overflow =1;
-        end    
-        else
-            overflow=0;
-    end       
     //multiply     
-    else if(opcode==3'b101)begin
+    if(opcode==3'b101 && pointer>=2)begin
         output_data=stack[pointer-1] * stack[pointer-2];
         checking_of=stack[pointer-1] * stack[pointer-2];
         extended_reg={{N{output_data[N-1]}},output_data};
@@ -28,6 +19,15 @@ always @(*)begin
         else
             overflow=0;
     end 
+    //addition
+    else if(opcode==3'b100 && (pointer>=2))begin
+        output_data=stack[pointer-1]+stack[pointer-2];
+        if((stack[pointer-1][N-1]==stack[pointer-2][N-1]) && (output_data[N-1]!= stack[pointer-1][N-1]))begin
+            overflow =1;
+        end    
+        else
+            overflow=0;
+    end       
     //push           
     else if(opcode==3'b110)begin
         stack[pointer]=input_data//put input in the current place of stack that pointer point into that;
@@ -35,8 +35,8 @@ always @(*)begin
     end
     //pop
     else if(opcode==3'111)begin
-        output_data=stack[pointer-1]//pick the amount of stack in perevious place;
         pointer=pointer-1;//dec pointer
+        output_data=stack[pointer]//pick the amount of stack in perevious place;
     end
 
 end
